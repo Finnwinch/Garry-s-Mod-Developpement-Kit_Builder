964c27cc837549f2129399d867f06b1a3a27ee0f
Léo <89333343+Finnwinch@users.noreply.github.com>
2024-01-20 19:07:55 -0500

    Update build_addons.sh
    update architecture, rename and some options

--- build_addons.sh
+++ build_addons.sh
@@ -1,5 +1,11 @@
-echo "Conceptualization VC Architecture - by finnwinch📎"
-read -p "Please enter the name of your addons" name
+#!/bin/bash
+echo "Conceptualization VCP (view-controler-partition) Architecture - by finnwinch📎"
+echo "VC Architecture was used to sperate action by level of design"
+read -p "Please enter the name of your script : " name
+cd ~/Steam/steamapps/common/GarrysModDS/garrysmod/addons #path about your server addons section
+while test -d $name ; do
+	read -p "Please enter a valide and unique name for your script : " name
+done
 mkdir $name
 cd $name
 mkdir lua
@@ -10,28 +16,48 @@ touch $name.src.lua #loader
 cd ..
 mkdir $name
 cd $name
-mkdir cache #cache color/materials and more
+mkdir cache #cache color/materials/any
+read -p "do you want to precache default color ? (empty for no) : " color_preselected
+if test ! -v $color_preselected ; then
+	cd cache
+	touch color.lua
+	color_lists="color_primary   = Color(0, 123, 255)
+color_secondary = Color(108, 117, 125)
+color_success   = Color(40, 167, 69)
+color_danger    = Color(220, 53, 69)
+color_warning   = Color(255, 193, 7)
+color_info      = Color(23, 162, 184)
+color_light     = Color(248, 249, 250)
+color_dark      = Color(33, 37, 41)
+color_greyDark  = Color(52, 58, 64)
+color_mauve     = Color(128, 92, 145)
+color_marine    = Color(60, 79, 118)"
+	echo "$color_lists" >> color.lua
+	cd ..
+fi
 mkdir class #class for management your addons
 mkdir connection #folder contain networking
 cd connection
-touch sv_base.lua #recive server example
-touch cl_base.lua #recive client example
-touch registre.lua #all your networking regist
+#touch sv_base.lua #recive server example
+#touch cl_base.lua #recive client example
+touch registre.lua #all your networking register
 cd ..
 mkdir database #code about your table / droptable
-mkdir event #yout hook used
-mkdir import #all file imported
-mkdir interface #folder contain interface
+mkdir event #your hook used
+mkdir import #all file imported, like framework
+mkdir interface #folder contain interface parts
 cd interface
 mkdir components #interaction about your layout
 mkdir container #elements into your layout
-mkdir layout #frame container
+mkdir layout #frame container with only first childen
+mkdir design #paint section !deprecreate, mayby, idk
 cd ..
 mkdir package #develop your own function to use
 mkdir sql #data access object
-mkdir config #folder contain config
-cd config
-touch $name.cfg.lua
-cd ..
-mkdir src #used for test implementation and final put data
-read -p "Conceptualization VC Architecture Finished!" closed
+#mkdir config #folder contain config
+#cd config
+#touch $name.cfg.lua
+#cd ..
+mkdir src #used for test implementation and final put data like pre config setup
+code ~/Steam/steamapps/common/GarrysModDS/garrysmod/addons/$name
+read -p "Conceptualization VCP Architecture Finished!" closed
